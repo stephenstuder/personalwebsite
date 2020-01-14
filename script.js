97f95a33a5103e245e2da9735bfb3ed459e22f6f
Stephen Studer <stephenkstuder@gmail.com>
2020-01-14 10:24:18 -0700

    fixed javascript files

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const swup = new Swup();
+
+
+//copy email address for the user
+const emailIcon = document.querySelector('.emailIcon');
+const email = 'stephenkstuder@gmail.com';
+
+emailIcon.addEventListener('click', function(){
+    navigator.clipboard.writeText(email).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+        alert('Email copied to your clipboard!');
+      }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+      });
+     
+});
+
